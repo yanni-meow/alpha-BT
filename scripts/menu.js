089759b0cceec8e8d535d94b141582b4935e3825
yanni <yanni.mamporiya@gmail.com>
2020-10-21 01:23:18 +0300

    index page

---
+++ scripts/menu.js
@@ -0,0 +1,23 @@
+// const menu = document.querySelectorAll('.menu__list__item');
+// const list = document.querySelectorAll('.sub-menu__list');
+
+// if(menu) {
+//     menu.forEach((element, i) => {
+//         element.addEventListener('click', function(event) {
+//             event.stopPropagation();
+//             list.forEach((el) => {
+//                 el.classList.remove('_active')
+//             })
+//             list[i].classList.add('_active');
+//         });
+//     });
+// };
+
+// document.body.addEventListener('click', function(event){
+//     if (event.target.className !== 'sub-menu__list') {
+//         list.forEach((el) => {
+//             el.classList.remove('_active')
+//         })    
+//     }
+// });
+
