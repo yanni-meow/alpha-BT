89bd9e3ef3cc8f20ad44e0e2b95b151e828bc178
yanni <yanni.mamporiya@gmail.com>
2020-10-21 23:04:31 +0300

    main pages & styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,4 @@
-// меню 
-
-const menu = document.querySelectorAll('.menu__list__item');
-const list = document.querySelectorAll('.sub-menu__list');
-
-if(menu) {
-    menu.forEach((element, i) => {
-        element.addEventListener('click', function(event) {
-            event.stopPropagation();
-            list.forEach((el) => {
-                el.classList.remove('_active')
-            })
-            // if(event.target.classList == '_active') {
-            //     element.classList.remove('_active')
-            // } else {
-            list[i].classList.toggle('_active');
-        //}
-        });
-    });
-};
-
-document.body.addEventListener('click', function(event){
-    if (event.target.className !== 'sub-menu__list') {
-        list.forEach((el) => {
-            el.classList.remove('_active')
-        })    
-    }
-});
-
-// анимация страницы
+// АНИМАЦИЯ при прокрутке
 
 const animItems = document.querySelectorAll('._anim-items');
 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,46 @@
-// const menu = document.querySelectorAll('.menu__list__item');
-// const list = document.querySelectorAll('.sub-menu__list');
+// МЕНЮ по ховеру
 
-// if(menu) {
-//     menu.forEach((element, i) => {
-//         element.addEventListener('click', function(event) {
-//             event.stopPropagation();
-//             list.forEach((el) => {
-//                 el.classList.remove('_active')
-//             })
-//             list[i].classList.add('_active');
-//         });
-//     });
-// };
+const menu = document.querySelectorAll('.menu__list__to-sub');
+const list = document.querySelectorAll('.sub-menu__list');
 
-// document.body.addEventListener('click', function(event){
-//     if (event.target.className !== 'sub-menu__list') {
-//         list.forEach((el) => {
-//             el.classList.remove('_active')
-//         })    
-//     }
-// });
+if(menu) {
+    menu.forEach((element, i) => {
+        element.addEventListener('mouseenter', function() {
+            list[i].classList.toggle('open');
+        });
+    });
+    menu.forEach((element, i) => {
+        element.addEventListener('mouseleave', function() {
+            list[i].classList.toggle('open');
+        });
+    });
+};
 
+// ПО КЛИКУ
+
+        // if(menu) {
+        //     menu.forEach((element, i) => {
+        //         element.addEventListener('click', function(event) {
+        //             event.stopPropagation();
+        //             // list.forEach((el) => {
+        //             //     el.classList.remove('open')
+        //             // })
+        //             if(event.target.classList == 'open') {
+        //                 element.classList.remove('open');
+        //             } else {
+        //                 list.forEach((el) => {
+        //                     el.classList.remove('open')
+        //                 });
+        //                 list[i].classList.toggle('open');
+        //             }
+        //         });
+        //     });
+        // };
+
+        // document.body.addEventListener('click', function(event){
+        //     if (event.target.className !== 'sub-menu__list') {
+        //         list.forEach((el) => {
+        //             el.classList.remove('open')
+        //         })    
+        //     }
+        // });
